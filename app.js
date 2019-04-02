3017a110eae55741c703d37b9a1af752a4330457
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-04-02 10:26:54 -0700

    add client pg code

--- app.js
+++ app.js
@@ -29,6 +29,23 @@ var events = [
 
 require('./controllers/events')(app, models);
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  // for (let row of res.rows) {
+  //   console.log(JSON.stringify(row));
+  // }
+  client.end();
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
